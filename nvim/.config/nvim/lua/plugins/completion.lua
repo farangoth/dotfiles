aa9c347a585064bb294fc12ccf55c500fae109a8
Clement Savalle-Anthonioz <savalle.clement@gmail.com>
2025-07-27 05:09:04 +0100

    remove double loading of LSP servers

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -48,13 +48,6 @@ return {
                 matching = { disallow_symbol_nonprefix_matching = false },
             })
 
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
-            require("lspconfig")["lua_ls"].setup({
-                capabilities = capabilities,
-            })
-            require("lspconfig")["pyright"].setup({
-                capabilities = capabilities,
-            })
         end,
     },
     {
