36d17bce35b58faf91e64d4e3b04d5ef56e2cf3e
Clement Savalle-Anthonioz <savalle.clement@gmail.com>
2025-07-29 10:12:08 +0100

    add CursorLine highlight

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,14 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+        require("catppuccin").setup({
+            flavour="macchiato"
+        })
         vim.cmd.colorscheme("catppuccin")
+        local colors = require("catppuccin.palettes").get_palette("macchiato")
+        vim.opt.cursorline = true
+        vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors["overlay0"] })
+        vim.api.nvim_set_hl(0, "CursorLineNr", { fg =  colors["lavender"] })
+        vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors["overlay0"]  })
     end,
 }
