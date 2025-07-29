0ef33ceafd27ad3e3feec5c7d47a00bab492e8f7
Clement Savalle-Anthonioz <savalle.clement@gmail.com>
2025-07-29 16:13:52 +0100

    update AI - add correct packages, but deactivate (for now)

---
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -0,0 +1,26 @@
+return {
+-- 	"yetone/avante.nvim",
+-- 	build = vim.fn.has("win32") and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+-- 		or "make",
+-- 	event = "VeryLazy",
+-- 	version = false,
+-- 	opts = {
+--         provider = "claude",
+--         providers = {
+--             claude = {
+--                 endpoint = "https://api.anthropic.com",
+--                 model = "claude-sonnet-4-20250514",
+--                 timeout = 30000, -- Timeout in milliseconds
+--                 extra_request_body = {
+--                     temperature = 0.75,
+--                     max_tokens = 20480,
+--                 },
+--             },
+--         }
+-- 	},
+-- 	dependencies = {
+-- 		"nvim-lua/plenary.nvim",
+-- 		"MunifTanjim/nui.nvim",
+-- 		"MeanderingProgrammer/render-markdown.nvim",
+-- 	},
+}
