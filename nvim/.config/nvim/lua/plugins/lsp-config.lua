cae61687cc27a4bd2c8e028c1f658dadb549e61b
Clement Savalle-Anthonioz <savalle.clement@gmail.com>
2025-07-31 06:08:22 +0100

    formatting file

--- nvim/.config/nvim/lua/plugins/lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/lsp-config.lua
@@ -1,56 +1,59 @@
 return {
-	{
-		"mason-org/mason.nvim",
-	},
+    {
+        "mason-org/mason.nvim",
+    },
 
-	{
-		"mason-org/mason-lspconfig.nvim",
-		lazy = false,
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-			"hrsh7th/cmp-nvim-lsp",
-		},
-		opts = {
-			ensure_installed = { "lua_ls", "pyright" },
-		},
-		config = function()
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "(LSP) Show Code Actions" })
-			vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, { desc = "(LSP) Rename" })
-			vim.keymap.set("n", "<leader>gc", vim.diagnostic.open_float, { desc = "(LSP) Show Diagnostics" })
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "(LSP) Go to Definition" })
-			vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "(LSP) Go to Implementation" })
-			vim.keymap.set("n", "<leader>gk", function()
-				vim.lsp.buf.hover({ border = "rounded" })
-			end, { desc = "(LSP) Show description" })
-			vim.keymap.set({ "i", "n" }, "<C-k>", vim.lsp.buf.signature_help, { desc = "(LSP) Go to Type Definition" })
+    {
+        "mason-org/mason-lspconfig.nvim",
+        lazy = false,
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        opts = {
+            ensure_installed = { "lua_ls", "pyright" },
+        },
+        config = function()
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "(LSP) Show Code Actions" })
+            vim.keymap.set("n", "<leader>gr", vim.lsp.buf.rename, { desc = "(LSP) Rename" })
+            vim.keymap.set("n", "<leader>gc", vim.diagnostic.open_float, { desc = "(LSP) Show Diagnostics" })
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, { desc = "(LSP) Go to Definition" })
+            vim.keymap.set("n", "<leader>gi", vim.lsp.buf.implementation, { desc = "(LSP) Go to Implementation" })
+            vim.keymap.set("n", "<leader>gk", function()
+                vim.lsp.buf.hover({ border = "rounded" })
+            end, { desc = "(LSP) Show description" })
+            vim.keymap.set({ "i", "n" }, "<C-k>", vim.lsp.buf.signature_help, { desc = "(LSP) Show signature" })
+            vim.keymap.set({ "n" }, "<leader>gs", function()
+                require("telescope.builtin").lsp_definitions()
+            end, { desc = "(LSP) Go to Source file" })
 
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			require("lspconfig")["lua_ls"].setup({
-				capabilities = capabilities,
-				settings = {},
-			})
-			require("lspconfig")["pyright"].setup({
-				capabilities = capabilities,
-				settings = {
-					python = {
-						analysis = {
-							-- typeCheckingMode = "strict",
-						},
-					},
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			vim.diagnostic.enable(true)
-			vim.diagnostic.config({ virtual_text = true })
-			vim.diagnostic.config({ update_in_insert = true })
-			vim.lsp.inlay_hint.enable(true)
-		end,
-	},
+            require("lspconfig")["lua_ls"].setup({
+                capabilities = capabilities,
+                settings = {},
+            })
+            require("lspconfig")["pyright"].setup({
+                capabilities = capabilities,
+                settings = {
+                    python = {
+                        analysis = {
+                            -- typeCheckingMode = "strict",
+                        },
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            vim.diagnostic.enable(true)
+            vim.diagnostic.config({ virtual_text = true })
+            vim.diagnostic.config({ update_in_insert = true })
+            vim.lsp.inlay_hint.enable(true)
+        end,
+    },
 }
