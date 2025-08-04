5cfbfd383ddf822e986b6e7f3ffe93842af657dc
Clement Savalle-Anthonioz <savalle.clement@gmail.com>
2025-08-04 14:46:29 +0100

    cleanup

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,11 +1,10 @@
-vim.cmd("set number")
-vim.cmd("set colorcolumn=88")
-vim.cmd("set noswapfile")
+vim.opt.number = true
+vim.opt.swapfile = false
 
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
