0c456817c578a0893c33607d872ee341d36b2f7e
Clement Savalle-Anthonioz <savalle.clement@gmail.com>
2025-08-14 20:57:02 +0100

    feat((nvim)): config to use the new version (main branch) of neovim-treesitter

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,16 +1,51 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    branch = 'main',
-    lazy = false,
-    build = ':TSUpdate',
-
-    config = function()
-        require('nvim-treesitter').setup({
-            auto_install = true,
-            highlight = {enable=true},
-            indent = {enable=true},
-        })
-    end
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		lazy = false,
+		build = {
+			function()
+				local parsers = {
+					"bash",
+					"c",
+					"cpp",
+                    "diff",
+					"gitcommit",
+					"gitignore",
+					"haskell",
+					"html",
+					"htmldjango",
+					"jq",
+					"json",
+					"latex",
+					"lua",
+					"make",
+					"markdown",
+					"ngimx",
+					"python",
+					"sql",
+					"ssh_config",
+					"vim",
+					"vimdoc",
+					"yaml",
+					"requirements",
+				}
+				require("nvim-treesitter").install(parsers)
+                require("nvim-treesitter").update()
+			end,
+		},
+        init = function()
+            vim.api.nvim_create_autocmd("FileType", {
+                callback = function(args)
+                    local filetype = args.match
+                    local lang = vim.treesitter.language.get_lang(filetype)
+                    if vim.treesitter.language.add(lang) then
+                        vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                        -- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                        vim.treesitter.start()
+                    end
+                end
+            })
+        end
+	},
 }
-
-
