39202c787e33270df66bdd9335e49ee7dcdbd9c1
Clement Savalle-Anthonioz <savalle.clement@gmail.com>
2025-09-17 21:40:48 +0100

    refactor(qtile):change shortcuts

--- qtile/.config/qtile/config.py
+++ qtile/.config/qtile/config.py
@@ -47,9 +47,7 @@ keys = [
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
     Key([mod], "tab", lazy.layout.next(), desc="Move window focus to other window"),
-    Key(
-        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
-    ),
+    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
     Key(
         [mod, "shift"],
         "l",
@@ -59,9 +57,7 @@ keys = [
     Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
     Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key(
-        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
-    ),
+    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
     Key([mod, "control"], "j", lazy.layout.growdown(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod, "Shift"], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
@@ -103,7 +99,8 @@ keys = [
     Key([mod], "e", lazy.spawn(file_explorer), desc="Open file_explorer"),
     Key([mod], "space", lazy.spawn("rofi -show combi"), desc="Show launcher menu"),
     Key([mod], "Escape", lazy.spawn("rofi -show power"), desc="Show power menu"),
-    Key([mod], "n", lazy.spawn("rofi -show notes"), desc="Show notes menu"),
+    Key([mod], "d", lazy.spawn("rofi -show notes"), desc="Show notes menu"),
+    Key([mod], "n", lazy.spawn(terminal + " nvim"), desc="Open NeoVim editor"),
     Key(
         [mod],
         "z",
@@ -431,9 +428,7 @@ mouse = [
         lazy.window.set_position_floating(),
         start=lazy.window.get_position(),
     ),
-    Drag(
-        [mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()
-    ),
+    Drag([mod], "Button3", lazy.window.set_size_floating(), start=lazy.window.get_size()),
     Click([mod], "Button2", lazy.window.bring_to_front()),
 ]
 
