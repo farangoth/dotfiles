7c0f03410f06c86648df403ed53145b14648e3a4
Clément Savalle-Anthonioz <savalle.clement@gmail.com>
2026-03-09 20:04:21 +0000

    (nvim) snippets

--- nvim/.config/nvim/lua/plugins/completion.lua
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -1,18 +1,46 @@
- vim.pack.add({
-	{
-		src = "https://github.com/Saghen/blink.cmp",
-		version = "v1.9.1",
-	},
-	{ 
-		src = "https://github.com/windwp/nvim-autopairs" 
-	},
+vim.pack.add({
+    {
+        src = "https://github.com/Saghen/blink.cmp",
+        version = "v1.9.1",
+    },
+    {
+        src = "https://github.com/windwp/nvim-autopairs"
+    },
+    {
+        src = "https://github.com/rafamadriz/friendly-snippets",
+    }
 })
 
 require("blink.cmp").setup({
-	keymap = { preset = "super-tab" },
-	appearance = {
-		nerd_font_variant = "mono",
-	},
+    keymap = {
+        preset = "super-tab",
+        -- ["<C-k>"] = {
+        --     "show_signature",
+        --     "hide_signature",
+        --     "fallback"
+        -- },
+        -- ["<C-K>"] = {
+        --     "show_documentation",
+        --     "hide_documentation",
+        --     "fallback"
+        -- }
+    },
+    completion = {
+        documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 500,
+        },
+    },
+    appearance = {
+        nerd_font_variant = "mono",
+    },
+    signature = {
+        enabled = true,
+        window = {
+            border = "rounded",
+            show_documentation = true
+        },
+    },
 })
 
 require("nvim-autopairs").setup({})
