8e783563fb76daa74cb7a8c394e59fc30381c30d
Clément Savalle-Anthonioz <savalle.clement@gmail.com>
2026-03-09 20:04:05 +0000

    (nvim) better keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,21 +1,25 @@
 local keymaps = {
     { "<leader>la", function() vim.lsp.buf.code_action() end, desc = "code actions" },
+    { "H",          ":bprevious<CR>",                         desc = "prev buffer",       silent = true },
+    { "L",          ":bnext<CR>",                             desc = "next buffer",       silent = true },
+    { "<leader>v",  ":vsplit<CR>",                            desc = "split vertical",    silent = true },
+    { "<leader>c",  ":hsplit<CR>",                            desc = "split horizontal",  silent = true },
+    { "<C-h>",      ":wincmd h<CR>",                          desc = "move to left win",  silent = true },
+    { "<C-j>",      ":wincmd j<CR>",                          desc = "move to down win",  silent = true },
+    { "<C-k>",      ":wincmd k<CR>",                          desc = "move to up win",    silent = true },
+    { "<C-l>",      ":wincmd l<CR>",                          desc = "move to right win", silent = true },
 }
 
 for _, map in ipairs(keymaps) do
-    local opts = { desc = map.desc }
-    if map.silent ~= nil then
-        opts.silent = map.silent
-    end
-    if map.noremap ~= nil then
-        opts.noremap = map.noremap
-    else
-        opts.noremap = true
-    end
-    if map.expr ~= nil then
-        opts.expr = map.expr
-    end
+    local lhs = map[1]
+    local rhs = map[2]
 
-    local mode = map.mode or "n"
-    vim.keymap.set(mode, map[1], map[2], opts)
+    local opts = vim.deepcopy(map)
+    opts[1] = nil
+    opts[2] = nil
+
+    local mode = opts.mode or "n"
+    opts.mode = nil
+    opts.noremap = opts.noremap ~= false
+    vim.keymap.set(mode, lhs, rhs, opts)
 end
